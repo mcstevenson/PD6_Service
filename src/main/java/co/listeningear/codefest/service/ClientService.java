19a7930d84a1e350af5332fae2416d850f52b0d9
dheeraj <dheerajjain004@gmail.com>
2023-10-11 16:50:21 +0100

    job and clint service  add

--- src/main/java/co/listeningear/codefest/service/ClientService.java
+++ src/main/java/co/listeningear/codefest/service/ClientService.java
@@ -1,18 +1,32 @@
 package co.listeningear.codefest.service;
 
 
+import co.listeningear.codefest.entity.ClientEntity;
 import co.listeningear.codefest.repository.ClientRepository;
-import co.listeningear.codefest.repository.JobRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Slf4j
 @Service
-public class ClientService {
+public class ClientService{
 
     @Autowired
     private ClientRepository clientRepository;
 
 
+    public ClientEntity save(ClientEntity entity) {
+        return clientRepository.save(entity);
+    }
+
+
+    public ClientEntity update(ClientEntity entity) {
+        return clientRepository.save(entity);
+    }
+
+    public Optional<ClientEntity> findById(Integer id) {
+        return clientRepository.findById(id);
+    }
 }
